6847696f826d86f6ce47758fa634a19a784f7030
the-predator072 <negivikas201@gmail.com>
2025-06-20 10:54:24 +0530

    fix: resolve startup issues preventing project from running

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -15,9 +15,9 @@ import { UsersModule } from '../users/users.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        secret: configService.get('jwt.secret'),
+        secret: configService.get('JWT_SECRET'),
         signOptions: {
-          expiresIn: configService.get('jwt.expiresIn'),
+          expiresIn: configService.get('JWT_EXPIRATION'),
         },
       }),
     }),
--- src/queues/scheduled-tasks/scheduled-tasks.module.ts
+++ src/queues/scheduled-tasks/scheduled-tasks.module.ts
@@ -3,10 +3,13 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { BullModule } from '@nestjs/bullmq';
 import { OverdueTasksService } from './overdue-tasks.service';
 import { TasksModule } from '../../modules/tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from '@modules/tasks/entities/task.entity';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    TypeOrmModule.forFeature([Task]),
     BullModule.registerQueue({
       name: 'task-processing',
     }),
